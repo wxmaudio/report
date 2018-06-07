faf7fac3f426b482879a375f62fff6e5d22c8f58
wxmaudio <wxm224@163.com>
2018-06-07 19:04:04 +0800

    添加富文本解析组件

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,4 +1,6 @@
 // pages/detail/index.js
+var WxParse = require('../../lib/wxParse/wxParse.js');
+
 Page({
 
   /**
@@ -8,7 +10,7 @@ Page({
     id: 1,
     title: '汽车节能对比',
     time: '2018-4-23',
-    content: 'werewrwerewcontent',
+    content: 'werewrwerewcontent<img src="http://n10.cmsfile.pg0.cn/group2/M00/3D/FF/Cgqg2VdTUp2AS7mfAADVjwW5fHQ920.jpg"/>',
     author:'beart'
   },
 
@@ -16,7 +18,8 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    let that = this;
+    WxParse.wxParse('content', 'html', that.data.content, that);
   },
 
   /**
