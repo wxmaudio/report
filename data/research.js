f56a4a82f5bd1f9d6a36083be7fe8482f3d58f0d
wxmaudio <wxm224@163.com>
2018-06-24 23:43:18 +0800

    添加后台数据通信

---
+++ data/research.js
@@ -0,0 +1,107 @@
+export default {
+  data: [
+    {
+      aid: "2247484907_1",
+      appmsgid: 2247484907,
+      icon: "https://mmbiz.qlogo.cn/mmbiz_jpg/Mw08flibRhVcmyZp1yZ1hib9lSt7wlGzUf0oibPficHKZxNcFiccCIaEw64yaQJGojgia3q6VqibhlX8AnDt9HolZsptA/0?wx_fmt=jpeg",
+      digest: "",
+      itemidx: 1,
+      link: "http://mp.weixin.qq.com/s?__biz=MzUzNDU5MDQxNg==&mid=2247484907&idx=1&sn=a03e1317ae964a7cd5be154b62623613&chksm=fa932354cde4aa42074fd90062b86a995b0f1ee082a403b413f041ae25fb86d47cedb485cd3e#rd",
+      title: "快充电动汽车全面分析——百亿市场，小快充崛起",
+      update_time: 1529409600
+    },
+    {
+      aid: "2247484886_1",
+      appmsgid: 2247484886,
+      icon: "https://mmbiz.qlogo.cn/mmbiz_jpg/Mw08flibRhVc9kaechhSXVfYdNdpFfBSJ93jIBHpGujYtVe9LYzmP6EpJdUdurFFDyemARFRS3EnH3ZCfd0vOHg/0?wx_fmt=jpeg",
+      digest: "",
+      itemidx: 1,
+      link: "http://mp.weixin.qq.com/s?__biz=MzUzNDU5MDQxNg==&mid=2247484886&idx=1&sn=ee16bd115114845d164ca9f6d3a21d20&chksm=fa932369cde4aa7f180a35307b777164d707f86ee72259f454fffac88ea2738a4739ef421a8f#rd",
+      title: "小米最全面分析——铁人三项，物联帝国！",
+      update_time: 1527075371
+    },
+    {
+      aid: "2247484765_1",
+      appmsgid: 2247484765,
+      icon: "https://mmbiz.qlogo.cn/mmbiz_jpg/Mw08flibRhVfR0zrOKW5ibGS1ic42ZDibwfM8nxwBGFm7VttXhJojY5NcIEibdMaibucRNRc7tlRkV92bhz35yLWNAmg/0?wx_fmt=jpeg",
+      digest: "",
+      itemidx: 1,
+      link: "http://mp.weixin.qq.com/s?__biz=MzUzNDU5MDQxNg==&mid=2247484765&idx=1&sn=784d9c11934aa1848d0ea1416f29d974&chksm=fa9323e2cde4aaf4ea6a1a6ede75bed8158017342fcc019a029f96ab65e8fb88d8dcb51ab6f6#rd",
+      title: "北京车展——电动车最全购车指南",
+      update_time: 1526388666
+    },
+    {
+      aid: "2247484733_1",
+      appmsgid: 2247484733,
+      icon: "https://mmbiz.qlogo.cn/mmbiz_jpg/Mw08flibRhVdIhSibx2ia5cO9qPfpOK7glxEpibJtL5N4K5UKSvfMn75fgIiajIXPtIcJ7UBlPXzXXqtERnd11FcARw/0?wx_fmt=jpeg",
+      digest: "2004 年，石墨烯首次发现，被称为“材料之王”，2010 年获得诺贝尔物理学奖，2025 年将达到千亿市场规模，我们认为：21世纪，人类将从硅时代全面迈向石墨烯时代。",
+      itemidx: 1,
+      link: "http://mp.weixin.qq.com/s?__biz=MzUzNDU5MDQxNg==&mid=2247484733&idx=1&sn=17afb84074578b29ed1af7bf40ac6526&chksm=fa932382cde4aa948faed7cb48f8ce180c045ee65f690048d786a96f3b42f6b20585ef3ffbae#rd",
+      title: "石墨烯最全面分析——人类将从硅时代迈向石墨烯时代",
+      update_time: 1525348800
+    },
+    {
+      aid: "2247484599_1",
+      appmsgid: 2247484599,
+      icon: "https://mmbiz.qlogo.cn/mmbiz_jpg/Mw08flibRhVfqiaoOXsnr7dzA1hS44mstmMba2sWsZLJ7a9G95KNjeuNicOFP4rWRA8VwXPhBSJOlXWjk9wIsoP0g/0?wx_fmt=jpeg",
+      digest: "",
+      itemidx: 1,
+      link: "http://mp.weixin.qq.com/s?__biz=MzUzNDU5MDQxNg==&mid=2247484599&idx=1&sn=cf18ed3d6cfb85e783243122512a3f55&chksm=fa932208cde4ab1e6dd16f152a57dd6e8ecb7ac14429ea3c2b685a9fdbf65ef910d9969c0889#rd",
+      title: "VR/AR 最全面分析——别了手机，未来已来！",
+      update_time: 1524571200
+    },
+    {
+      aid: "2247484484_1",
+      appmsgid: 2247484484,
+      icon: "https://mmbiz.qlogo.cn/mmbiz_jpg/Mw08flibRhVeGNyQJrQbobcxaC1PntJ8Luh1SQAzliboCPy9NXUGdZMRSicRSwA9JlEBrBW7HKGzBicHPUAOEIESpw/0?wx_fmt=jpeg",
+      digest: "以史为鉴：贸易战从未能解决逆差问题，合则两利，斗则俱伤；战略藐视：全球分工不同是逆差来源，韬光养晦永不过时，2027 年有望完成中华民族伟大复兴；战术重视：史上 5 次 301 调查都是谈判手段，适当反击，专注技术升级是核心要义。",
+      itemidx: 1,
+      link: "http://mp.weixin.qq.com/s?__biz=MzUzNDU5MDQxNg==&mid=2247484484&idx=1&sn=f9a9c9eb2fda53afd97e1545109b4299&chksm=fa9322fbcde4abed2bca3b5769455d66fe6cc24d29878cb14867c886c5aaa18596e374ca6514#rd",
+      title: "一文终结贸易战：以史为鉴，战略藐视，战术重视",
+      update_time: 1523880000
+    },
+    {
+      aid: "2247484430_1",
+      appmsgid: 2247484430,
+      icon: "https://mmbiz.qlogo.cn/mmbiz_jpg/Mw08flibRhVfiaic7icakGRDa9aE914BjAvSESgtSszbIcpCD8KHVMksw33BvIvIq4gDnFXoUgn5vIOPDCVt4yr8Eg/0?wx_fmt=jpeg",
+      digest: "2017总结&2018展望（一）：乘用车 2017总结&2018展望（二）：客车 2017总结&2018展望（三）：电池 2017总结&2018展望（四）：专用车 请关注公众号查看历史消息。",
+      itemidx: 1,
+      link: "http://mp.weixin.qq.com/s?__biz=MzUzNDU5MDQxNg==&mid=2247484430&idx=1&sn=54336ee3e97521ac2f1686a4362d287f&chksm=fa9322b1cde4aba7938363e2893f973f1f38b8f7d342fee6328fc821c104c0a8dd5a6882d70e#rd",
+      title: "2017年总结&2018年展望（五）：货车/卡车",
+      update_time: 1523188800
+    },
+    {
+      aid: "2247484426_1",
+      appmsgid: 2247484426,
+      icon: "https://mmbiz.qlogo.cn/mmbiz_jpg/Mw08flibRhVcNurgn01Wv9CwAeF8OtvrY34hQO8afnFxSsdY8wM4UdmtWgbicsXibGO76mUbHy2a7CJXj6leSPYaQ/0?wx_fmt=jpeg",
+      digest: "2018 年，新能源汽车动力电池将进入规模化退役阶段，2020 年，锂电回收市场规模预计将达到 150 亿，年均复合增长率超过 50%，将成为新能源汽车产业链下一个风口。",
+      itemidx: 1,
+      link: "http://mp.weixin.qq.com/s?__biz=MzUzNDU5MDQxNg==&mid=2247484426&idx=1&sn=e4bc2631fb5119db13541392e490dadc&chksm=fa9322b5cde4aba39ba8c1f11aa839469d79638a643aa384f83cfca7acd5e3141f2c5e806db6#rd",
+      title: "锂电回收最全面分析：下一个风口？",
+      update_time: 1522670400
+    },
+    {
+      aid: "2247484373_1",
+      appmsgid: 2247484373,
+      icon: "https://mmbiz.qlogo.cn/mmbiz_jpg/Mw08flibRhVfkQtDV8sNWmfFhoxIH1JrrdcepWhyubG8icA2rfqmw3VapRnehozqibjtxy8yMzFHFeicr3ibL0g7Y1Q/0?wx_fmt=jpeg",
+      digest: "3 年利润翻 80 倍，7 年估值千亿，秉承技术为王的曾毓群连续抓住了智能手机和新能源汽车两大风口，造就了 ATL 和 CATL 两大全球龙头企业，堪称大国重器。",
+      itemidx: 1,
+      link: "http://mp.weixin.qq.com/s?__biz=MzUzNDU5MDQxNg==&mid=2247484373&idx=1&sn=2a721b523dd7f32fd472937eb7ff5ae4&chksm=fa93256acde4ac7c3f5d2ca6cc47ed47f4bd5a8a0b46aa9c6e8afee3a42de9c0fba12811c6d4#rd",
+      title: "CATL 宁德时代最全面分析：大国重器，全球龙头！",
+      update_time: 1522065600
+    },
+    {
+      aid: "2247484371_1",
+      appmsgid: 2247484371,
+      icon: "https://mmbiz.qlogo.cn/mmbiz_jpg/Mw08flibRhVdNaVKRfcokT6aRoukmAfibZYgtPtDe1BYZxy4ghXqZF1EvV9ibboIgW3rsJ9pXhtpgdUMd6asb002Q/0?wx_fmt=jpeg",
+      digest: "",
+      itemidx: 1,
+      link: "http://mp.weixin.qq.com/s?__biz=MzUzNDU5MDQxNg==&mid=2247484371&idx=1&sn=b32388c3d6a12c38f37c3aa0ec2d1d07&chksm=fa93256ccde4ac7ac8507bfd740dae9db99f4e7faf5b972e9d564ad594f14fecb30b17972b8f#rd",
+      title: "中国市值第一，日赚 2 亿，帝国诞生！",
+      update_time: 1521720000
+    }
+  ],
+  "code": 0,
+  "msg": ""
+}
+
--- pages/research/research.js
+++ pages/research/research.js
@@ -1,20 +1,47 @@
 // research.js
 //获取应用实例
 const app = getApp();
+const RESEARCH_URL = "http://47.104.85.144/article?pageNum=1&pageSize=2&catId=1";
+function loadList(url, params, context, callback) {
+  wx.request({
+    url: url,
+    data: params,
+    header: {
+      'content-type': 'application/json'
+    },
+    success: function (res) {   
+      console.log(res, res.data); 
+      context.setData({ list: res.data.data});
+      callback();
+    }
+  });
+}
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    list: [
+    list: []/*[
       {
-        nid: "http://www.lovelycn.cn/stwm/detail/image/20180517/1000600000015041526520960129422207_1.html",
-        videoApi: null,
+        "id": 2,
+        "title": "发到手",
+        "author": "发送到",
+        "originalAuthor": "反倒是",
+        "icon": "http://ljljljkjkjljkj",
+        "writeType": 2,
+        "catId": 123,
+        "writeTime": "2018-06-23T11:24:04.000+0000",
+        "updateTime": "2018-06-23T11:24:26.000+0000",
+        "createdTime": "2018-06-23T11:24:26.000+0000"
+      },
+      {
+        id: "http://www.lovelycn.cn/stwm/detail/image/20180517/1000600000015041526520960129422207_1.html",
         title: "废弃物装扮美丽乡村废弃物装扮美丽乡村废弃物装扮美丽乡村废弃物装扮美丽乡村废弃物装扮美丽乡村",
         description: "",
-        picture: "http://img1.daguan.com/group1/M00/18/70/CgrZE1r83H-Ad_tqAAMNerH8VKw419.jpg",
-        mname: "浙江在线",
+        icon: "http://img1.daguan.com/group1/M00/18/70/CgrZE1r83H-Ad_tqAAMNerH8VKw419.jpg",
+        originalAuthor: "浙江在线",
         time: "2018-05-17 09:36:00",
         duration: "",
         letvUrl: null,
@@ -22,35 +49,37 @@ Page({
         type: "3",
         newsType: "5",
         tag: "浙江,农村,美丽乡村,装饰",
-        pictureList: null,
-        url: "http://m.lovelycn.cn/app_detail.html?nid=8E4A912C54052FC52A33AF5038F3330B054543FD7F3F732806F7DD57A0621A31A2E12D9B2EB4A57176B072BBE24AEA92B0E7A13519A9A50836D6A1B734F531AD8C5B33C452AFE3CEB66CC52E94AD0506BE2D6952A5F72D404BB069FF93F945EC",
+        url: "http://m.lovelycn.cn/app_detail.html?id=8E4A912C54052FC52A33AF5038F3330B054543FD7F3F732806F7DD57A0621A31A2E12D9B2EB4A57176B072BBE24AEA92B0E7A13519A9A50836D6A1B734F531AD8C5B33C452AFE3CEB66CC52E94AD0506BE2D6952A5F72D404BB069FF93F945EC",
         videoUrl: ""
       }, {
-        nid: "http://www.lovelycn.cn/stwm/detail/image/20180517/1000600000015041526520960129422207_1.html",
+        id: "http://www.lovelycn.cn/stwm/detail/image/20180517/1000600000015041526520960129422207_1.html",
         videoApi: null,
         title: "废弃物装扮美丽乡村666",
         description: "",
-        picture: "http://img1.daguan.com/group1/M00/18/70/CgrZE1r83H-Ad_tqAAMNerH8VKw419.jpg",
-        mname: "浙江在线",
+        icon: "http://img1.daguan.com/group1/M00/18/70/CgrZE1r83H-Ad_tqAAMNerH8VKw419.jpg",
+        originalAuthor: "浙江在线",
         time: "2018-05-17 09:36:00",
-        duration: "",
-        letvUrl: null,
-        leshidianbo: null,
         type: "3",
         newsType: "5",
-        tag: "浙江,农村,美丽乡村,装饰",
-        pictureList: null,
-        url: "http://m.lovelycn.cn/app_detail.html?nid=8E4A912C54052FC52A33AF5038F3330B054543FD7F3F732806F7DD57A0621A31A2E12D9B2EB4A57176B072BBE24AEA92B0E7A13519A9A50836D6A1B734F531AD8C5B33C452AFE3CEB66CC52E94AD0506BE2D6952A5F72D404BB069FF93F945EC",
-        videoUrl: ""
+        url: "http://m.lovelycn.cn/app_detail.html?id=8E4A912C54052FC52A33AF5038F3330B054543FD7F3F732806F7DD57A0621A31A2E12D9B2EB4A57176B072BBE24AEA92B0E7A13519A9A50836D6A1B734F531AD8C5B33C452AFE3CEB66CC52E94AD0506BE2D6952A5F72D404BB069FF93F945EC"
       }
-    ]
+    ]*/
   },
   
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    wx.showLoading({
+      title: '加载中...',
+    })
+    loadList(RESEARCH_URL, {
+      pageNum: 1,
+      pageSize: 2,
+      catId: 1
+    }, this, function() {
+      wx.hideLoading();
+    });
   },
 
   /**
